21ecb1a5626073c9c6f91d5e8c05c6ed4f13568c
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-03-02 11:32:14 +0400

    alphabet check

---
+++ alphabet check.py
@@ -0,0 +1,5 @@
+letter=input("enter a single character: ")
+if letter=="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"y"or"z":
+    print("Your input is part of the alphabet")
+elif letter ==1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
+    print("your input is not part of the alphabet")
